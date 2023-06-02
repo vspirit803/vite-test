6f6313f1e748fda77308610482804613906ba948
vspirit803 <lx546488173@gmail.com>
2023-06-02 13:59:58 +0800

    init

---
+++ packages/core/src/index.ts
@@ -0,0 +1,6 @@
+import { hello } from "@core/utils";
+
+export function add(a: number, b: number) {
+  hello()
+  return a + b;
+}
---
+++ packages/core/src/utils/index.ts
@@ -0,0 +1,5 @@
+import { helloStr } from "@core/constants/hello"
+
+export function hello() {
+    console.log(helloStr);
+}
